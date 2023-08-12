3dc95acaac6660334e5e6894ca0fbfca8b6f5b93
Devloper-solaiman <devlopersolaiman@gmail.com>
2023-08-12 22:27:56 +0600

    first commit

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-
 require('dotenv').config();
 
 const port = process.env.PORT || 5000;
 const app = express();
-
-
 app.use(cors());
 app.use(express.json());
 
@@ -46,8 +43,6 @@ async function run() {
             const user = await userCollection.findOne(query)
             res.send(user)
         })
-
-
         app.put('/product/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) };
@@ -99,19 +94,13 @@ async function run() {
         })
 
     }
-    finally {
-
-
-    }
-
+    finally { }
 
 }
 run().catch(console.dir);
-
 app.get('/', (req, res) => {
     res.send('running assaginment 11 sarver')
 });
-
 app.listen(port, () => {
     console.log(`installing server port assaginment-11, ${port}`);
 })
